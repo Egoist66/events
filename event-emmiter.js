a69d5a6df15a1def022ff9597b4e11b9187eee82
Farid Mahmudov <Razormad666@gmail.com>
2025-01-21 16:12:31 +0300

    update

--- event-emmiter.js
+++ event-emmiter.js
@@ -1,5 +1,28 @@
+/**
+ * Creates a new custom event with the specified name and detail.
+ *
+ * @param {string} name - The name of the custom event.
+ * @param {Object} detail - An object containing additional data to pass with the event.
+ * @returns {CustomEvent} The newly created custom event.
+ */
+
 function createCustomEvent(name, detail) {
     return new CustomEvent(name, { detail })
+}
+
+/**
+ * Adds a key-value pair to the detail object.
+ * 
+ * @param {Object} detail - The object to which the key-value pair will be added.
+ * @param {Object} param1 - An object containing the key and value to add.
+ * @param {string} param1.key - The key to add to the detail object.
+ * @param {*} param1.value - The value associated with the key.
+ * @returns {Object} The updated detail object.
+ */
+
+export function toDetail(detail, {key, value}) {
+    detail[key] = value
+    return detail
 }
 
 /**
@@ -13,9 +36,10 @@ function createCustomEvent(name, detail) {
  * @param {*} options.detail The detail of the custom event.
  * @param {function} options.handler The handler for the custom event.
  */
-export function emit({element, triggerEventName, eventName, detail, handler}) {
+export function emit({element, triggerEventName, eventName, detail, handler = () => {}}) {
     const event = createCustomEvent(eventName, detail)
-    element.addEventListener(eventName, handler)
+
+    element.addEventListener(eventName, (e) => handler(e, event.detail))
     element.addEventListener(triggerEventName, () => {
         element.dispatchEvent(event)
     })
--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-import { emit } from "./event-emmiter.js";
+import { emit, toDetail } from "./event-emmiter.js";
 
 emit({
     element: document.querySelector("button"),
     triggerEventName: "click",
     eventName: "alert",
-    detail: { name: "John Doe" },
-    handler: async (e) => {
-        console.log("Event emitted", e);
+    detail: { todos: 1 },
+    handler: async (e, detail) => {
+        console.log(detail);
         if(e.type === 'alert'){
 
-            const todo = await(await fetch('https://jsonplaceholder.typicode.com/todos/1')).json();
+            const todo = await(await fetch(`https://jsonplaceholder.typicode.com/todos/${detail.todos}`)).json();
             console.log(todo);
+            
         }
     },
 });
+
+emit({
+    element: document.body,
+    triggerEventName: 'dblclick',
+    eventName: 'double-tap',
+    detail: {bgColor: 'red'},
+    handler(e, detail){
+
+        e.target.style.backgroundColor = detail.bgColor;
+
+        console.log(detail);
+
+        const changeBackgroundColor = () => {
+            toDetail(detail, {
+                key: 'bgColor',
+                value: `#${Math.floor(Math.random() * 16777215).toString(16)}`
+            });
+
+            e.target.style.backgroundColor = detail.bgColor;
+        };
+
+        const interval = setInterval(changeBackgroundColor, 1000);
+
+        window.addEventListener('blur', () => {
+            document.title = 'Blur';
+            clearInterval(interval);
+        });
+
+
+
+    
+
+      
+    }
+})
